c7f26b4b0e4d335786573a01c9190d0b896db507
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-07-03 22:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateSize() {
+      var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+      var sizeTextElement = document.getElementById("sizeText");
+      sizeTextElement.textContent = "Width: " + width + " Height: " + height;
+    }
+
+    // Initial call to update the size information
+    updateSize();
+
+    // Event listener for window resize
+    window.addEventListener("resize", updateSize);
